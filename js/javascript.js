4653c516aff8aca06b1b538242972d9c3302cdaf
zacarias-sebast <140893755+zacarias-sebast@users.noreply.github.com>
2023-12-07 04:44:42 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+/*
+let menubar = document.querySelector('#menu-bar');
+let mynav = document.querySelector('.navbar');
+
+menubar.onclick = () =>{
+
+    mynav.classList.toggle('active');
+}
+
+var msgCookies = document.getElementById("cookies-msg");
+
+function aceito(){
+     localStorage.lgpd = "sim"
+     msgCookies.classList.remove('monstrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+} else{
+    msgCookies.classList.add('mostrar')
+}
+/*
+
+
